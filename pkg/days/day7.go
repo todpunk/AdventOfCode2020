9cac955ccc795c0b9e5787f9a6c59b49a71a5e27
Tod Hansmann <githubpub@todandlorna.com>
2020-12-11 22:07:33 -0700

    added day 7

--- pkg/days/day7.go
+++ pkg/days/day7.go
@@ -1,9 +1,75 @@
 package days
 
+import (
+	"strconv"
+	"strings"
+)
+
+type bag_rule struct {
+	color string
+	count int64
+}
+
 func Day7a(input []string) int {
-	return 4
+	var my_color = "shiny gold"
+	var bags = map[string][]bag_rule{}
+	var contained_in = map[string][]bag_rule{}
+
+	for _, rule := range input {
+		var splits = strings.Split(rule, " bags contain ")
+		bags[splits[0]] = []bag_rule{}
+		for _, rule_part := range strings.Split(splits[1], ", ") {
+			var bag = rule_part[:strings.Index(rule_part, " bag")]
+			if bag != "no other" {
+				var bag_num, _ = strconv.ParseInt(bag[:strings.Index(bag, " ")], 10, 64)
+				var bag_name = bag[strings.Index(bag, " ")+1:]
+				bags[splits[0]] = append(bags[splits[0]], bag_rule{bag_name, bag_num})
+				if _, ok := contained_in[bag_name]; !ok {
+					contained_in[bag_name] = []bag_rule{}
+				}
+				contained_in[bag_name] = append(contained_in[bag_name], bag_rule{splits[0], bag_num})
+			}
+		}
+	}
+
+	var my_bags = contained_in[my_color]
+	var seen = map[string]bool{}
+	for i := 0; i < len(my_bags); i++ {
+		for _, next_bag := range contained_in[my_bags[i].color] {
+			if _, ok := seen[next_bag.color]; !ok {
+				my_bags = append(my_bags, next_bag)
+				seen[next_bag.color] = true
+			}
+		}
+	}
+	return len(my_bags)
 }
 
 func Day7b(input []string) int {
-	return 4
+	var my_color = "shiny gold"
+	var bags = map[string][]bag_rule{}
+
+	for _, rule := range input {
+		var splits = strings.Split(rule, " bags contain ")
+		bags[splits[0]] = []bag_rule{}
+		for _, rule_part := range strings.Split(splits[1], ", ") {
+			var bag = rule_part[:strings.Index(rule_part, " bag")]
+			if bag != "no other" {
+				var bag_num, _ = strconv.ParseInt(bag[:strings.Index(bag, " ")], 10, 64)
+				var bag_name = bag[strings.Index(bag, " ")+1:]
+				bags[splits[0]] = append(bags[splits[0]], bag_rule{bag_name, bag_num})
+			}
+		}
+	}
+
+	var my_bags = []bag_rule{bag_rule{my_color, 0}}
+	var n int64 = 0
+	for i := 0; i < len(my_bags); i++ {
+		for _, next_bag := range bags[my_bags[i].color] {
+			for n = 0; n < next_bag.count; n++ {
+				my_bags = append(my_bags, next_bag)
+			}
+		}
+	}
+	return len(my_bags) - 1
 }
--- test/days_test/day7_test.go
+++ test/days_test/day7_test.go
@@ -8,17 +8,38 @@ import (
 
 func TestDay7(t *testing.T) {
 	var example = []string{
-		"abc",
-		"",
+		"light red bags contain 1 bright white bag, 2 muted yellow bags.",
+		"dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+		"bright white bags contain 1 shiny gold bag.",
+		"muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+		"shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+		"dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+		"vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+		"faded blue bags contain no other bags.",
+		"dotted black bags contain no other bags.",
 	}
 	var result int = days.Day7a(example)
-	if result != 5 {
+	if result != 4 {
 		t.Fail()
-		t.Logf("Day7a example should return 5, got %d", result)
+		t.Logf("Day7a example should return 4, got %d", result)
 	}
 	result = days.Day7b(example)
-	if result != 5 {
+	if result != 32 {
 		t.Fail()
-		t.Logf("Day7b example should return 5, got %d", result)
+		t.Logf("Day7b example should return 32, got %d", result)
+	}
+	var example2 = []string{
+		"shiny gold bags contain 2 dark red bags.",
+		"dark red bags contain 2 dark orange bags.",
+		"dark orange bags contain 2 dark yellow bags.",
+		"dark yellow bags contain 2 dark green bags.",
+		"dark green bags contain 2 dark blue bags.",
+		"dark blue bags contain 2 dark violet bags.",
+		"dark violet bags contain no other bags.",
+	}
+	result = days.Day7b(example2)
+	if result != 126 {
+		t.Fail()
+		t.Logf("Day7b example2 should return 126, got %d", result)
 	}
 }
