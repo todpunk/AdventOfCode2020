adb6abe3ccdbc2f7181add4f1c624dc798ca24a5
Tod Hansmann <githubpub@todandlorna.com>
2020-12-11 23:43:40 -0700

    add day9 part 1

--- pkg/days/day9.go
+++ pkg/days/day9.go
@@ -1,9 +1,24 @@
 package days
 
-func Day9a(input []string) int {
-	return 4
+func Day9a(input []int, preambleLength int) int {
+	var found = 0
+	for i := preambleLength; i < len(input) && found == 0; i++ {
+		var target = input[i]
+		var valid = false
+		for j := i - preambleLength; j < i-1 && !valid; j++ {
+			for k := j + 1; k < i && !valid; k++ {
+				if input[j]+input[k] == target {
+					valid = true
+				}
+			}
+		}
+		if !valid {
+			found = input[i]
+		}
+	}
+	return found
 }
 
-func Day9b(input []string) int {
+func Day9b(input []int) int {
 	return 4
 }
--- test/days_test/day9_test.go
+++ test/days_test/day9_test.go
@@ -7,14 +7,32 @@ import (
 )
 
 func TestDay9(t *testing.T) {
-	var example = []string{
-		"abc",
-		"",
+	var example = []int{
+		35,
+		20,
+		15,
+		25,
+		47,
+		40,
+		62,
+		55,
+		65,
+		95,
+		102,
+		117,
+		150,
+		182,
+		127,
+		219,
+		299,
+		277,
+		309,
+		576,
 	}
-	var result int = days.Day9a(example)
-	if result != 5 {
+	var result int = days.Day9a(example, 5)
+	if result != 127 {
 		t.Fail()
-		t.Logf("Day9a example should return 5, got %d", result)
+		t.Logf("Day9a example should return 127, got %d", result)
 	}
 	result = days.Day9b(example)
 	if result != 5 {
