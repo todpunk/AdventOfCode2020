a8f58b47e97f6bfd55dc17f7026df0c23b2f791f
Tod Hansmann <githubpub@todandlorna.com>
2020-12-13 23:29:35 -0700

    add day14 part 1

--- pkg/days/day14.go
+++ pkg/days/day14.go
@@ -1,7 +1,39 @@
 package days
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 func Day14a(input []string) int {
-	return 4
+	var mem = map[uint64]uint64{}
+	var mask = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+	var total uint64
+	for _, line := range input {
+		if line[:3] == "mas" {
+			mask = strings.Split(line, " = ")[1]
+		}
+		if line[:3] == "mem" {
+			address, _ := strconv.ParseUint(line[4:strings.Index(line, "]")], 10, 64)
+			val, _ := strconv.ParseUint(strings.Split(line, " = ")[1], 10, 64)
+			strval := fmt.Sprintf("%036v", strconv.FormatUint(val, 2))
+			// fmt.Println(strval)
+			for i, override := range mask {
+				if override == '1' || override == '0' {
+					strval = strings.Join([]string{strval[:i], string(override), strval[i+1:]}, "")
+				}
+			}
+			// fmt.Println(mask)
+			// fmt.Println(strval)
+			mem[address], _ = strconv.ParseUint(strval, 2, 64)
+			// fmt.Println(mem[address])
+		}
+	}
+	for _, val := range mem {
+		total = total + val
+	}
+	return int(total)
 }
 
 func Day14b(input []string) int {
--- test/days_test/day14_test.go
+++ test/days_test/day14_test.go
@@ -8,13 +8,15 @@ import (
 
 func TestDay14(t *testing.T) {
 	var example = []string{
-		"abc",
-		"",
+		"mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X",
+		"mem[8] = 11",
+		"mem[7] = 101",
+		"mem[8] = 0",
 	}
 	var result int = days.Day14a(example)
-	if result != 5 {
+	if result != 165 {
 		t.Fail()
-		t.Logf("Day14a example should return 5, got %d", result)
+		t.Logf("Day14a example should return 165, got %d", result)
 	}
 	result = days.Day14b(example)
 	if result != 5 {
