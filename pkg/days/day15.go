d1d3f55e456ede7bc9182f63fb54a43ca09dc10b
Tod Hansmann <githubpub@todandlorna.com>
2020-12-15 06:42:55 -0700

    add day 15

--- pkg/days/day15.go
+++ pkg/days/day15.go
@@ -1,9 +1,59 @@
 package days
 
+import (
+	"strconv"
+	"strings"
+)
+
+type day15Counter struct {
+	turn  int
+	count int
+}
+
+func play15Game(input string, nth_number int) int {
+	var primers = map[int]*day15Counter{}
+	var turn int = 1
+	var last_spoken = 0
+	for _, val := range strings.Split(input, ",") {
+		num, _ := strconv.ParseInt(val, 10, 64)
+		primers[int(num)] = &day15Counter{turn, 0}
+		last_spoken = int(num)
+		turn++
+	}
+	for ; turn <= nth_number; turn++ {
+		prev_spokens_turn := primers[last_spoken].turn
+		primers[last_spoken].turn = turn - 1
+		if primers[last_spoken].count == 0 {
+			// fmt.Println("Last Spoken was new:", last_spoken, "turn:", primers[last_spoken].turn, primers[last_spoken].count)
+			primers[last_spoken].count++
+			if _, ok := primers[0]; !ok {
+				primers[0] = &day15Counter{turn, 0}
+			} else {
+				primers[0].count += 1
+			}
+			last_spoken = 0
+			// fmt.Println("New Spoken is: 0")
+		} else {
+			// fmt.Println("Last Spoken isn't new:", last_spoken, "turn:", primers[last_spoken].turn, primers[last_spoken].count)
+			primers[last_spoken].count++
+			primers[last_spoken].turn = turn - 1
+			new_spoken := turn - 1 - prev_spokens_turn
+			// fmt.Println("New Spoken is:", new_spoken)
+			if _, ok := primers[new_spoken]; !ok {
+				primers[new_spoken] = &day15Counter{turn, 0}
+			} else {
+				primers[new_spoken].count += 1
+			}
+			last_spoken = new_spoken
+		}
+	}
+	return last_spoken
+}
+
 func Day15a(input []string) int {
-	return 4
+	return play15Game(input[0], 2020)
 }
 
 func Day15b(input []string) int {
-	return 4
+	return play15Game(input[0], 30000000)
 }
--- test/days_test/day15_test.go
+++ test/days_test/day15_test.go
@@ -7,18 +7,38 @@ import (
 )
 
 func TestDay15(t *testing.T) {
-	var example = []string{
-		"abc",
-		"",
+	var example = map[int][]string{
+		436:  []string{"0,3,6"},
+		1:    []string{"1,3,2"},
+		10:   []string{"2,1,3"},
+		27:   []string{"1,2,3"},
+		78:   []string{"2,3,1"},
+		438:  []string{"3,2,1"},
+		1836: []string{"3,1,2"},
 	}
-	var result int = days.Day15a(example)
-	if result != 5 {
-		t.Fail()
-		t.Logf("Day15a example should return 5, got %d", result)
+	for k, v := range example {
+		// if k == 436 {
+		var result int = days.Day15a(v)
+		if result != k {
+			t.Fail()
+			t.Logf("Day15a example should return %d, got %d", k, result)
+		}
+		// }
 	}
-	result = days.Day15b(example)
-	if result != 5 {
-		t.Fail()
-		t.Logf("Day15b example should return 5, got %d", result)
+	example = map[int][]string{
+		175594:  []string{"0,3,6"},
+		2578:    []string{"1,3,2"},
+		3544142: []string{"2,1,3"},
+		261214:  []string{"1,2,3"},
+		6895259: []string{"2,3,1"},
+		18:      []string{"3,2,1"},
+		362:     []string{"3,1,2"},
+	}
+	for k, v := range example {
+		result := days.Day15b(v)
+		if result != k {
+			t.Fail()
+			t.Logf("Day15b example should return %d, got %d", k, result)
+		}
 	}
 }
